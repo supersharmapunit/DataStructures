a7fef3a10c332691f6feb961742edd8dd209f01b
supersharmapunit <punit2000sharma@gmail.com>
2021-04-13 23:22:04 +0530

    .

--- construct/Client.java
+++ construct/Client.java
@@ -1,34 +1,13 @@
 public class Client {
 
-    public static int divideSolve(int a, int b) throws Exception {
-        if (b == 0) {
-            throw new Exception("CanNotDivideByZero");
-        }
-        return a / b;
-    }
-
-    public static int divide(int a, int b) {
-        int ans = -1;
-
-        try {
-            ans = divideSolve(a, b);
-        } catch (Exception e) {
-            System.out.println("INFI");
+    public static void main(String[] args) throws Exception {
+        stack st = new stack();
+        for (int i = 0; i < 10; i++) {
+        st.push(i + 10);
         }
 
-        return ans;
-    }
-
-    public static void main(String[] args) {
-        // stack st = new stack();
-        // for (int i = 0; i < 10; i++) {
-        // st.push(i + 10);
-        // }
-
-        // while (st.size() != 0) {
-        // System.out.print(st.pop());
-        // }
-
-        System.out.println(divide(100, 0));
+        while (st.size() != 0) {
+        System.out.print(st.pop() + " ");
+        }
     }
 }
--- construct/Stack.java
+++ construct/Stack.java
@@ -1,24 +1,74 @@
-public class Stack {
+public class stack {
 
-    
+    private int[] arr;
+    private int tos;
+    private int noOfElements;
+    private int maxCapacity;
+
+    protected void initialize(int size) {
+        this.arr = new int[size];
+        this.tos = -1;
+        this.noOfElements = 0;
+        this.maxCapacity = size;
+    }
+
+    public stack(){
+        initialize(15);
+    }
+
+    public stack(int size){
+        initialize(size);
+    }
     
     public int size() {
-        return -1;
+        return this.noOfElements;
+    }
+
+    protected int Capacity(){
+        return this.maxCapacity;
     }
 
     public boolean isEmpty() {
-        return false;
+        return this.noOfElements == 0;
     }
 
-    public void push(int data) {
-
+    protected void StackEmptyException() throws Exception {
+        if (this.noOfElements == 0) throw new Exception("StackIsEmpty");
+    }
+    protected void StackOverflow() throws Exception {
+        if (this.noOfElements == this.maxCapacity) throw new Exception("StackOverflow");
     }
 
-    public int top() {
-        return -1;
+    protected void push_(int data) {
+        this.tos++;
+        this.arr[this.tos] = data;
+        this.noOfElements++;
     }
 
-    public int pop() {
-        return -1;
+    public void push(int data) throws Exception {
+        StackOverflow();
+        push_(data);
+    }
+
+    protected int top_() {
+        return this.arr[this.tos];
+    }
+
+    public int top() throws Exception {
+        StackEmptyException();
+        return top_();
+    }
+
+    protected int  pop_() {
+        int rv = this.arr[this.tos];
+        this.arr[this.tos] = 0;
+        this.tos--;
+        this.noOfElements--;
+        return rv;
+    }
+
+    public int pop() throws Exception {
+        StackEmptyException();
+        return pop_();
     }
 }
