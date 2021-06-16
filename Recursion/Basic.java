ab9c718cd17001c2cb80bfaf14cc9855104958a1
Punit Sharma <punit2000sharma@gmail.com>
2021-06-16 20:33:21 +0530

    update

--- Recursion/Basic.java
+++ Recursion/Basic.java
@@ -2,131 +2,145 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Basic {
-    public static void printIncreasing(int a, int b){
-        if (a > b) return;
+    public static void printIncreasing(int a, int b) {
+        if (a > b)
+            return;
 
         System.out.println(a);
-        printIncreasing(a+1, b);
+        printIncreasing(a + 1, b);
     }
 
-    public static void printDecreasing(int a, int b){
-        if (a > b) return;
+    public static void printDecreasing(int a, int b) {
+        if (a > b)
+            return;
 
-        printDecreasing(a+1, b);
-        System.out.println(a);
-    }
-
-    public static void printIncreasingDecreasing(int a, int b){
-        if (a > b) return;
-
-        System.out.println(a);
-        printIncreasingDecreasing(a+1, b);
+        printDecreasing(a + 1, b);
         System.out.println(a);
     }
 
-    public static void printEvenOdd(int a, int b){
-        if (a > b) return;
+    public static void printIncreasingDecreasing(int a, int b) {
+        if (a > b)
+            return;
 
-        if ( a % 2 == 0) System.out.println(a);
-        printEvenOdd(a+1, b);
-        if (a % 2 != 0) System.out.println(a);
+        System.out.println(a);
+        printIncreasingDecreasing(a + 1, b);
+        System.out.println(a);
     }
 
-    public static int fib(int a){
-        if (a <= 1) return 1;
+    public static void printEvenOdd(int a, int b) {
+        if (a > b)
+            return;
+
+        if (a % 2 == 0)
+            System.out.println(a);
+        printEvenOdd(a + 1, b);
+        if (a % 2 != 0)
+            System.out.println(a);
+    }
+
+    public static int fib(int a) {
+        if (a <= 1)
+            return 1;
 
         return fib(a - 1) * a;
     }
 
-    public static int maximumInArray(int[] arr, int idx){
-        if (idx == arr.length) return -(int)1e9;
+    public static int maximumInArray(int[] arr, int idx) {
+        if (idx == arr.length)
+            return -(int) 1e9;
 
-        int maxSf = maximumInArray(arr, idx+1);
+        int maxSf = maximumInArray(arr, idx + 1);
 
         return Math.max(arr[idx], maxSf);
 
     }
 
-    public static int minimumInArray(int[] arr, int idx){
-        if (idx == arr.length) return (int)1e9;
+    public static int minimumInArray(int[] arr, int idx) {
+        if (idx == arr.length)
+            return (int) 1e9;
 
         int minSF = minimumInArray(arr, idx + 1);
 
         return Math.min(arr[idx], minSF);
     }
 
-    public static boolean findInArray(int[] arr, int idx, int data){
-        if (idx == arr.length) return false;      
+    public static boolean findInArray(int[] arr, int idx, int data) {
+        if (idx == arr.length)
+            return false;
 
-        return arr[idx] == data || findInArray(arr, idx+1 , data);
-        
+        return arr[idx] == data || findInArray(arr, idx + 1, data);
+
     }
 
-    public static int firstIDX(int[] arr, int idx, int data){
+    public static int firstIDX(int[] arr, int idx, int data) {
         if (idx == arr.length)
             return -1;
-        
-        return arr[idx] == data ? idx : firstIDX(arr, idx+1 , data);
-        
+
+        return arr[idx] == data ? idx : firstIDX(arr, idx + 1, data);
+
     }
-    
-    
-    public static int lastIDX(int[] arr, int idx, int data){
-        if (idx == arr.length){
+
+    public static int lastIDX(int[] arr, int idx, int data) {
+        if (idx == arr.length) {
             return -1;
         }
 
-        int smallans = lastIDX(arr, idx+1, data);
+        int smallans = lastIDX(arr, idx + 1, data);
 
-        if (smallans != -1) return smallans;
+        if (smallans != -1)
+            return smallans;
 
         return arr[idx] == data ? idx : -1;
     }
 
-    public static boolean isSorted(int[] arr, int n){
-        if(n == 1 || n == 0) return true;
-        
-        return arr[n - 1] < arr[n - 2] ? false : isSorted(arr, n-1);
+    public static boolean isSorted(int[] arr, int n) {
+        if (n == 1 || n == 0)
+            return true;
+
+        return arr[n - 1] < arr[n - 2] ? false : isSorted(arr, n - 1);
     }
 
-    public static int power(int a, int b){
-        if (b < 1) return 1;
+    public static int power(int a, int b) {
+        if (b < 1)
+            return 1;
 
-        return a * power(a, b-1);
+        return a * power(a, b - 1);
     }
 
-    public static int powerOpti(int a, int b){
-        if (b == 0) return 1;
-        int rres = power(a, b/2);
+    public static int powerOpti(int a, int b) {
+        if (b == 0)
+            return 1;
+        int rres = power(a, b / 2);
         rres *= rres;
         return b % 2 == 0 ? rres : rres * a;
     }
 
-    public static int numberOfDigits(int n, int count){
-        if (n/10 < 10) return 1;
-        numberOfDigits(n%10, count++);
+    public static int numberOfDigits(int n, int count) {
+        if (n / 10 < 10)
+            return 1;
+        numberOfDigits(n % 10, count++);
         return count;
     }
 
-    public static void display(int[] arr, int idx){
-        if (idx == arr.length){
+    public static void display(int[] arr, int idx) {
+        if (idx == arr.length) {
             return;
         }
 
         System.out.print(arr[idx] + " ");
-        display(arr, idx+1);
+        display(arr, idx + 1);
     }
 
-    public static void displayReverse(int[] arr, int idx){
-        if (idx == arr.length){
+    public static void displayReverse(int[] arr, int idx) {
+        if (idx == arr.length) {
             return;
         }
 
-        displayReverse(arr, idx+1);
+        displayReverse(arr, idx + 1);
         System.out.print(arr[idx] + " ");
     }
 
-    public static int printTreePaths(int n){
+    public static int printTreePaths(int n) {
         if (n == 0 || n == 1) {
             System.out.println("base" + n);
             return n;
@@ -134,33 +148,36 @@ public class Basic {
 
         int ans = 0;
 
-        System.out.println("Pre: " + n );
-        ans += printTreePaths(n -1);
+        System.out.println("Pre: " + n);
+        ans += printTreePaths(n - 1);
 
-        System.out.println("In: " + n);;
+        System.out.println("In: " + n);
+        ;
 
-        ans += printTreePaths(n-2);
+        ans += printTreePaths(n - 2);
         System.out.println("Post: " + n);
 
         return ans + 3;
 
     }
 
-    public static int[] allIndex(int[] arr,  int idx, int count, int data){
-        if(idx == arr.length){
+    public static int[] allIndex(int[] arr, int idx, int count, int data) {
+        if (idx == arr.length) {
             return new int[count];
         }
-        if (arr[idx] == data) count++;
+        if (arr[idx] == data)
+            count++;
 
         int[] psf = allIndex(arr, idx + 1, count, data);
 
-        if (arr[idx] == data) psf[--count] = idx;
+        if (arr[idx] == data)
+            psf[--count] = idx;
 
         return psf;
     }
 
-    public static ArrayList<String> subsequenceStr_1(String str, int idx){
-        if(idx == str.length()){
+    public static ArrayList<String> subsequenceStr_1(String str, int idx) {
+        if (idx == str.length()) {
             ArrayList<String> base = new ArrayList<>();
             base.add("");
             return base;
@@ -170,60 +187,49 @@ public class Basic {
         ArrayList<String> myAns = new ArrayList<>(recAns); // automatically adds recAns to the ArrayList
         // in which there are elements without the first character
         // now when that char will be included -->
-        for(String ele : recAns)
+        for (String ele : recAns)
             myAns.add(str.charAt(idx) + ele);
-        
-        return myAns; 
+
+        return myAns;
     }
 
-    public static int subsequenceStr_2(String str, int idx, String ans, ArrayList<String> res){
-        if(idx == str.length()){
+    public static int subsequenceStr_2(String str, int idx, String ans, ArrayList<String> res) {
+        if (idx == str.length()) {
             res.add(ans);
-            System.out.print(" "+ans);
+            System.out.print(" " + ans);
             return 1;
         }
 
         int count = 0;
 
-        count += subsequenceStr_2(str, idx+1, ans+str.charAt(idx), res);
-        count += subsequenceStr_2(str, idx+1, ans, res);
+        count += subsequenceStr_2(str, idx + 1, ans + str.charAt(idx), res);
+        count += subsequenceStr_2(str, idx + 1, ans, res);
 
         return count;
     }
 
     // GET KEYPAD PATTERNS
-    static String keypad[] = {
-        ".;",
-        "abc",
-        "def",
-        "ghi",
-        "jkl",
-        "mno",
-        "pqrs",
-        "tuv",
-        "wxyz",
-        " "
-    };
+    static String keypad[] = { ".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz", " " };
 
-    public static ArrayList < String > getKPC(String ques) {
-        if (ques.length() == 0 ){
+    public static ArrayList<String> getKPC(String ques) {
+        if (ques.length() == 0) {
             ArrayList<String> base = new ArrayList<String>();
             base.add("");
             return base;
         }
         char ch = ques.charAt(0);
-        String roq = ques.substring(1); //roq = rest of the ques
-        
+        String roq = ques.substring(1); // roq = rest of the ques
+
         ArrayList<String> rres = getKPC(roq);
-        
+
         int idx = ch - '0';
         String word = keypad[idx];
         ArrayList<String> mylist = new ArrayList<String>();
-        
-        for (int i = 0; i < word.length(); i++){
+
+        for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            for (String s : rres){
-                mylist.add(c+s);
+            for (String s : rres) {
+                mylist.add(c + s);
             }
         }
         return mylist;
@@ -263,79 +269,80 @@ public class Basic {
         return myAns;
     }
 
-    public static ArrayList<String> mazePathHVDmulti(int sr, int sc, int er, int ec){
-        if(sr == er && sc == ec){
+    public static ArrayList<String> mazePathHVDmulti(int sr, int sc, int er, int ec) {
+        if (sr == er && sc == ec) {
             ArrayList<String> base = new ArrayList<>();
             base.add("");
             return base;
         }
-        
 
         ArrayList<String> myans = new ArrayList<>();
-        
 
-        for (int jumps = 1; sc + jumps <= ec; jumps++){
-            ArrayList<String> horizontal = mazePathHVDmulti(sr, sc+jumps, er, ec);
-            for (String ele : horizontal) myans.add(ele + "h" + jumps);
+        for (int jumps = 1; sc + jumps <= ec; jumps++) {
+            ArrayList<String> horizontal = mazePathHVDmulti(sr, sc + jumps, er, ec);
+            for (String ele : horizontal)
+                myans.add(ele + "h" + jumps);
         }
 
-        for (int jumps = 1; sr + jumps <= er; jumps++){
+        for (int jumps = 1; sr + jumps <= er; jumps++) {
             ArrayList<String> vertical = mazePathHVDmulti(sr + jumps, sc, er, ec);
-            for (String ele : vertical) myans.add(ele + "v" + jumps);
+            for (String ele : vertical)
+                myans.add(ele + "v" + jumps);
         }
 
-        for (int jumps = 1; sr + jumps <= er && sc + jumps <= ec ; jumps++){
-            ArrayList<String> diagonal = mazePathHVDmulti(sr + jumps, sc+jumps, er, ec);
-            for (String ele : diagonal) myans.add(ele + "d" + jumps);
+        for (int jumps = 1; sr + jumps <= er && sc + jumps <= ec; jumps++) {
+            ArrayList<String> diagonal = mazePathHVDmulti(sr + jumps, sc + jumps, er, ec);
+            for (String ele : diagonal)
+                myans.add(ele + "d" + jumps);
         }
 
         return myans;
     }
-    
+
     // bottom to top
-    public static int mazePathHVD2(int sr, int sc, int er, int ec, String psf,ArrayList<String> ans){
-        if(sr == ec && sc == ec){
+    public static int mazePathHVD2(int sr, int sc, int er, int ec, String psf, ArrayList<String> ans) {
+        if (sr == ec && sc == ec) {
             ans.add(psf);
             return 1;
         }
         int count = 0;
-        if (sc+1  <= ec ){
-            count += mazePathHVD2(sr, sc+1, er, ec, psf + "h", ans);
+        if (sc + 1 <= ec) {
+            count += mazePathHVD2(sr, sc + 1, er, ec, psf + "h", ans);
         }
-        if (sr+1 <= er ){
-            count += mazePathHVD2(sr+1, sc, er, ec, psf + "v", ans);          
+        if (sr + 1 <= er) {
+            count += mazePathHVD2(sr + 1, sc, er, ec, psf + "v", ans);
         }
-        if (sr+1 <= er && sc+1 <= ec){
-            count += mazePathHVD2(sr+1, sc+1, er, ec, psf + "d", ans);
+        if (sr + 1 <= er && sc + 1 <= ec) {
+            count += mazePathHVD2(sr + 1, sc + 1, er, ec, psf + "d", ans);
         }
 
         return count;
 
     }
 
-    public static int mazePathHVD2multi(int sr, int sc, int er, int ec, String psf, ArrayList<String> ans){
-        if(sr == er && sc == ec){
+    public static int mazePathHVD2multi(int sr, int sc, int er, int ec, String psf, ArrayList<String> ans) {
+        if (sr == er && sc == ec) {
             ans.add(psf);
             return 1;
         }
 
         int count = 0;
 
-        for (int jumps = 1; sc + jumps <= ec; jumps++){
+        for (int jumps = 1; sc + jumps <= ec; jumps++) {
             count += mazePathHVD2multi(sr, sc + jumps, er, ec, psf + "h" + jumps, ans);
         }
-        for (int jumps = 1; sr + jumps <= er; jumps++){
-            count += mazePathHVD2multi(sr + jumps, sc , er, ec, psf + "v" + jumps, ans);
+        for (int jumps = 1; sr + jumps <= er; jumps++) {
+            count += mazePathHVD2multi(sr + jumps, sc, er, ec, psf + "v" + jumps, ans);
         }
-        for (int jumps = 1; sc + jumps <= ec && sr + jumps <= er; jumps++){
+        for (int jumps = 1; sc + jumps <= ec && sr + jumps <= er; jumps++) {
             count += mazePathHVD2multi(sr + jumps, sc + jumps, er, ec, psf + "d" + jumps, ans);
         }
 
         return count;
     }
 
-    public static int mazePathHVD2DirOptimized(int sr, int sc, int er, int ec, int[][] dir, String[] dirS, ArrayList<String> ans,
-            String psf) {
+    public static int mazePathHVD2DirOptimized(int sr, int sc, int er, int ec, int[][] dir, String[] dirS,
+            ArrayList<String> ans, String psf) {
         if (sr == er && sc == ec) {
             ans.add(psf);
             return 1;
@@ -353,21 +360,20 @@ public class Basic {
         return count;
     }
 
-    public static void mazePathsAll(){
+    public static void mazePathsAll() {
         ArrayList<String> ans = new ArrayList<>();
         ArrayList<String> ans1 = new ArrayList<>();
 
-        int[][] dir = {{0,1}, {1,0}, {1,1}};
-        String[] dirS = {"h", "v", "d"};
-        
-        
+        int[][] dir = { { 0, 1 }, { 1, 0 }, { 1, 1 } };
+        String[] dirS = { "h", "v", "d" };
+
         System.out.println(mazePathHVD2DirOptimized(0, 0, 2, 2, dir, dirS, ans, ""));
         System.out.println(mazePathHVD2(0, 0, 2, 2, "", ans1));
         System.out.println(ans);
         System.out.println(ans1);
     }
 
-    public static void floodfillAlgorithm(){
+    public static void floodfillAlgorithm() {
         int sr = 0, sc = 0, n = 3, m = 3;
         boolean[][] vis = new boolean[n][m];
         // int[][] dir = { { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1
@@ -383,7 +389,6 @@ public class Basic {
 
         System.out.println(ans);
     }
-    
 
     public static int floodFill(int sr, int sc, boolean[][] vis, int[][] dir, String[] dirS, ArrayList<String> ans,
             String psf) {
@@ -451,12 +456,12 @@ public class Basic {
         vis[sr][sc] = true;
 
         for (int rad = 1; rad <= Math.max(n, m); rad++) {
-        for (int d = 0; d < dir.length; d++){
+            for (int d = 0; d < dir.length; d++) {
                 int r = sr + rad * dir[d][0];
                 int c = sc + rad * dir[d][1];
 
                 if (r >= 0 && c >= 0 && r < n && c < m && !vis[r][c]) {
-                        count += floodFill_multiCircular(r, c, vis, dir, dirS, ans, psf + dirS[d] + rad);
+                    count += floodFill_multiCircular(r, c, vis, dir, dirS, ans, psf + dirS[d] + rad);
                 }
             }
         }
@@ -465,8 +470,82 @@ public class Basic {
         return count;
     }
 
+    public static class pair {
+        String psf = "";
+        int len = 0;
 
-    public static void  main(String args[]){
+        pair(String psf, int len) {
+            this.len = len;
+            this.psf = psf;
+        }
+    }
+
+    public static pair longestPath(int sr, int sc, boolean[][] vis, int[][] dir, String[] dirS) {
+        int n = vis.length, m = vis[0].length;
+        if (sr == n - 1 && sc == m - 1) {
+            return new pair("", 0);
+        }
+
+        vis[sr][sc] = true; // blocked
+        pair myAns = new pair("", -1);
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m) {
+                if (!vis[r][c]) {
+                    pair recAns = longestPath(r, c, vis, dir, dirS);
+                    if (recAns.len != -1 && recAns.len + 1 > myAns.len) {
+                        myAns.len = recAns.len + 1;
+                        myAns.psf = dirS[d] + recAns.psf;
+                    }
+                }
+            }
+        }
+
+        vis[sr][sc] = false; // unblocked
+        return myAns;
+    }
+
+    public static pair shortestPath(int sr, int sc, boolean[][] vis, int[][] dir, String[] dirS) {
+        int n = vis.length, m = vis[0].length;
+        if (sr == n - 1 && sc == m - 1) {
+            return new pair("", 0);
+        }
+
+        vis[sr][sc] = true; // blocked
+        pair myAns = new pair("", (int) 1e9);
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m) {
+                if (!vis[r][c]) {
+                    pair recAns = shortestPath(r, c, vis, dir, dirS);
+                    if (recAns.len != (int) 1e9 && recAns.len + 1 < myAns.len) {
+                        myAns.len = recAns.len + 1;
+                        myAns.psf = dirS[d] + recAns.psf;
+                    }
+                }
+            }
+        }
+
+        vis[sr][sc] = false; // unblocked
+        return myAns;
+    }
+
+    public static void longestShortestPath() {
+        int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        String[] dirS = { "D", "R", "U", "L" };
+
+        boolean[][] vis = new boolean[3][3];
+        // vis[1][1] = vis[1][2] = vis[2][1] = true;
+
+        pair ans = longestPath(0, 0, vis, dir, dirS);
+        System.out.println(ans.psf + " @ " + ans.len);
+    }
+
+    public static void main(String args[]) {
         floodfillAlgorithm();
 
     }
--- Recursion/Questions.java
+++ Recursion/Questions.java
@@ -1,20 +1,22 @@
-public class Questions{
+public class Questions {
     // https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1
 
     public static ArrayList<String> findPath(int[][] m, int n) {
         ArrayList<String> res = new ArrayList<>();
-        
-        if (m[0][0] == 0 || m[n - 1][n - 1] == 0) return res;
-        
-        String[] dirS = {"D","L","R", "U"};
-        int[][] dir = {{1,0},{0,-1},{0,1},{-1,0}};
-        
-        int moves = fill(0,0,m,res,"",dir,dirS);
+
+        if (m[0][0] == 0 || m[n - 1][n - 1] == 0)
+            return res;
+
+        String[] dirS = { "D", "L", "R", "U" };
+        int[][] dir = { { 1, 0 }, { 0, -1 }, { 0, 1 }, { -1, 0 } };
+
+        int moves = fill(0, 0, m, res, "", dir, dirS);
         return res;
     }
 
     // exact flood fill with minor changes according to ques
-    public static int fill(int sr, int sc, int[][] vis, ArrayList<String> res, String psf, int[][] dir, String[] dirS){
+    public static int floodfill(int sr, int sc, int[][] vis, ArrayList<String> res, String psf, int[][] dir,
+            String[] dirS) {
         int n = vis.length, m = vis[0].length;
         if (sr == n - 1 && sc == m - 1) {
             res.add(psf);
@@ -28,7 +30,7 @@ public class Questions{
             int c = sc + dir[d][1];
 
             if (r >= 0 && c >= 0 && r < n && c < m && vis[r][c] == 1) {
-                count += fill(r, c, vis, res,psf + dirS[d], dir, dirS);
+                count += fill(r, c, vis, res, psf + dirS[d], dir, dirS);
             }
         }
 
@@ -36,8 +38,98 @@ public class Questions{
         return count;
     }
 
-
     // https://practice.geeksforgeeks.org/problems/special-matrix4201/1
+    // will give TLE here but it will be solved using DP
+    public int FindWays(int n, int m, int[][] arr) {
+        int vis[][] = new int[n + 1][m + 1];
+        for (int[] ele : arr)
+            vis[ele[0]][ele[1]] = 1;
+
+        if (vis[1][1] == 1 || vis[n][m] == 1)
+            return 0;
+
+        int[][] dir = { { 0, 1 }, { 1, 0 } };
+        int moves = floodfill(1, 1, vis, dir);
+        return moves;
+    }
+
     // https://www.geeksforgeeks.org/rat-in-a-maze-with-multiple-steps-jump-allowed/?ref=rp
+    public void display1D(int[] arr) {
+        for (int ele : arr)
+            System.out.print(ele + " ");
+
+        System.out.println();
+    }
+
+    public void display2D(int[][] arr) {
+        for (int ele : arr)
+            display1D(a);
+
+        System.out.println();
+    }
+
+    public int ratInMazeWithJumps(int sr, int sc, int[][] jumpMat, int[][] dir, int[][] ans) {
+        int n = jumpMat.length, m = jumpMat[0].length;
+        if (sr == n - 1 && sc == m - 1) {
+            ans[sr][sc] = 1;
+            display2D(ans);
+            ans[sr][sc] = 0;
+            return 1;
+        }
+
+        int jump = jumpMat[sr][sc];
+        jumpMat[sr][sc] = 0; // block
+        ans[sr][sc] = 1; // psf
+
+        int count = 0;
+        for (int d = 0; d < dir.length; d++) {
+            for (int rad = 1; rad <= jump; rad++) {
+                int r = sr + rad * dir[d][0];
+                int c = sc + rad * dir[d][1];
+
+                if (r >= 0 && c >= 0 && r < n && c < m) {
+                    if (jumpMat[r][c] != 0)
+                        count += floodFill(r, c, jumpMat, dir, ans);
+                } else
+                    break;
+            }
+        }
+
+        jumpMat[sr][sc] = jump; // unblock
+        ans[sr][sc] = 0; // psf
+        return count;
+    }
+
+    public int longestPath(int sr, int sc, int[][] arr, String ans, String psf, boolean[][] visited, int[][] dir, String[] dirS){
+        int n = vis.length, m = vis[0].length;
+
+        if (sr == n - 1 && sc == m - 1){
+            if (psf.size() > ans.size()) ans = psf;
+            else continue;
+            return 1;
+        }
+
+        int count = 0;
+        vis[sr][sc] = true;
+
+
+        for (int d = 0; d < dir.length; d++){
+            for (int jumps = 1; jumps < Math.max(m,n); jumps++){
+                int r = sr + jumps + dir[d][0];
+                int c = sc + jumps + dir[d][1];
+
+                if (r >= 0 && c >= 0 && r < n && c < m){
+                    if (vis[r][c] == false){
+                        count += longestPath(r, c, arr, ans, psf + dirS[d] + jumps, visited, dir, dirS);
+                    }
+                }
+
+            }
+        }
+
+
+        vis[sr][sc] = false;
+        return count;
+    }
 
 }
