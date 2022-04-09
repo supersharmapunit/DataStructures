9cca38876e9b6905a2e0bc525da2ca280ab58d91
Punit Sharma <punit2000sharma@gmail.com>
2022-04-09 15:29:44 +0530

    crytAirthematic

--- Recursion/QueenSeries.java
+++ Recursion/QueenSeries.java
@@ -252,9 +252,10 @@ public class QueenSeries {
         return wordBreakSolver(s, "", wd);
     }
 
-    public boolean wordBreakSolver(String str, String asf, HashSet<String> wordDict) { 
+    public boolean wordBreakSolver(String str, String asf, HashSet<String> wordDict) {
         // eg-> str = ilovemangoandsamsung
-        // wordDict = ["i", "ilove", "love", "man", "mango","go","and","sam", "sung", "samsung"]
+        // wordDict = ["i", "ilove", "love", "man", "mango","go","and","sam", "sung",
+        // "samsung"]
         int n = str.length();
         if (n == 0)
             return true;
@@ -263,13 +264,92 @@ public class QueenSeries {
             String smallStr = str.substring(0, len);
 
             if (wordDict.contains(smallStr)) { // contains ilove so we'll make a call
-                if (wordBreakSolver(str.substring(len), asf + smallStr + " ", wordDict)){
+                if (wordBreakSolver(str.substring(len), asf + smallStr + " ", wordDict)) {
                     // call for str.substring(len) ilovemangoandsamsung -> mangoandsamsung
                     return true;
-                } 
+                }
             }
         }
         return false;
+    }
+
+    // cryptAirthematic eg -> send+more = money
+    // https://see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf
+    static String str1 = "send", str2 = "more", str3 = "money";
+    static boolean[] isNumUsed = new boolean[10];
+    static int[] mapping = new int[26];
+    
+    public static void crypto() {
+        String str = str1 + str2 + str3;
+        int[] freq = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i) - 'a']++;
+        }
+
+        str = "";
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0)
+                str += (char) (i + 'a');
+        }
+
+        if (str.length() > 10)
+            return;
+
+        System.out.println(crypto(str, 0));
+    }
+
+    private static int stringToNumber(String str) {
+        int res = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            res = res * 10 + mapping[ch - 'a'];
+        }
+
+        return res;
+    }
+
+    private static boolean isValidMapping() {
+        if (mapping[str1.charAt(0) - 'a'] == 0 || mapping[str2.charAt(0) - 'a'] == 0
+                || mapping[str3.charAt(0) - 'a'] == 0)
+            return false;
+
+        int num1 = stringToNumber(str1);
+        int num2 = stringToNumber(str2);
+        int num3 = stringToNumber(str3);
+
+        return num1 + num2 == num3;
+    }
+
+    public static int crypto(String str, int idx) {
+        if (idx == str.length()) {
+            if (isValidMapping()) {
+                for (int i = 0; i < str.length(); i++) {
+                    char ch = str.charAt(i);
+                    System.out.print(ch + "=" + mapping[ch - 'a'] + ", ");
+                }
+                System.out.println();
+                return 1;
+            }
+
+            return 0;
+        }
+
+        int count = 0;
+        char ch = str.charAt(idx);
+        for (int num = 0; num <= 9; num++) {
+            if (!isNumUsed[num]) {
+                isNumUsed[num] = true;
+                mapping[ch - 'a'] = num;
+
+                count += crypto(str, idx + 1);
+
+                mapping[ch - 'a'] = 0;
+                isNumUsed[num] = false;
+            }
+
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
@@ -288,6 +368,7 @@ public class QueenSeries {
         // System.out.println("count : " + count);
         // int count = nQueens01Combination(new boolean[4][4], 4, 0, "");
         // System.out.println("count : " + count);
+        crypto();
     }
 
     public static void display1D(int[] arr) {
