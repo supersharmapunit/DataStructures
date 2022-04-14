43eff985fbd68cb0c42c7b1821280ecb335efabf
Punit Sharma <punit2000sharma@gmail.com>
2022-04-14 10:14:58 +0530

    few more questions on LL

--- LinkedList/LinkedList01.java
+++ LinkedList/LinkedList01.java
@@ -68,11 +68,6 @@ public class LinkedList01 {
         return prev;
     }
 
-    // LC21
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return null;
-    }
-
     // LC234
     public boolean isPalindrome(ListNode head) {
         if(head == null || head.next == null) return true;
@@ -87,7 +82,8 @@ public class LinkedList01 {
         
         ListNode l1 = head, l2 = newHead;
         boolean res = true;
-        while(l2 != null){
+        while(l2 != null){ // we haven't wrote l1 != null because in the case of breaking of odd sized
+        // LL l2 will be the ptr that will qpproach null first and in even case it will approach same
             if(l1.val != l2.val){
                 res = false;
                 break;
@@ -106,6 +102,76 @@ public class LinkedList01 {
 
     // LC143
     public void reorderList(ListNode head) {
-
+        if(head == null || head.next == null) return;
+        
+        ListNode mid = middleNode_01(head), nHead = mid.next;
+        mid.next = null;
+        nHead = reverseList(nHead);
+        
+        ListNode c1 = head, c2 = nHead;
+        
+        
+        while(c2 != null){
+            ListNode fwd1 = c1.next, fwd2 = c2.next;
+            
+            c1.next = c2;
+            c2.next = fwd1;
+            
+            c1 = fwd1;
+            c2 = fwd2;
+        }
     }
+
+    public int size(ListNode head){
+        if(head == null) return 0;
+        int count = 0;
+        ListNode temp = head;
+        while(temp != null) {
+            temp = temp.next;
+             count++;
+        }
+        return count;
+    }
+
+    public void swap(ListNode l1, ListNode l2){
+        ListNode temp = new ListNode();
+        temp.val = l1.val;
+        temp.next = l1.next;
+
+        l1.val = l2.val;
+        l1.next = l2.next;
+
+        l2.val = temp.val;
+        l2.next = temp.next;
+    }
+
+    public ListNode getNodeAtIdx(ListNode node, int idx){
+        ListNode temp = node;
+        while(idx-- >= 0){
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    // LC21
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null || list2 == null) return list1 == null ? list2 : list1;
+
+        ListNode dummy = new ListNode(-1), prev = dummy, ptr1 = list1, ptr2 = list2;
+
+        while (ptr1 != null && ptr2 != null){
+            if(ptr1.val <= ptr2.val){
+                prev.next = ptr1;
+                ptr1 = ptr1.next;
+            } else {
+                prev.next = ptr2;
+                ptr2 = ptr2.next;
+            }
+
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
 }
