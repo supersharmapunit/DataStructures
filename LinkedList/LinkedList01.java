1fc92eb1b0399afd5ab317e435cb12a002010afa
Punit Sharma <punit2000sharma@gmail.com>
2022-04-20 01:13:23 +0530

    LinkedListQuestions

--- LinkedList/LinkedList01.java
+++ LinkedList/LinkedList01.java
@@ -70,70 +70,74 @@ public class LinkedList01 {
 
     // LC234
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null) return true;
-        
-//         1.we'll find the middleNode
-//         2.then we'll reverse the list from the middle
-//         3.traverse in both the list if something unequal found then false otherwise true
-        
+        if (head == null || head.next == null)
+            return true;
+
+        // 1.we'll find the middleNode
+        // 2.then we'll reverse the list from the middle
+        // 3.traverse in both the list if something unequal found then false otherwise
+        // true
+
         ListNode mid = middleNode_01(head), newHead = mid.next;
         mid.next = null; // breaking the list so that we have two seperate lists while we do the work
         newHead = reverseList(newHead);
-        
+
         ListNode l1 = head, l2 = newHead;
         boolean res = true;
-        while(l2 != null){ // we haven't wrote l1 != null because in the case of breaking of odd sized
-        // LL l2 will be the ptr that will qpproach null first and in even case it will approach same
-            if(l1.val != l2.val){
+        while (l2 != null) { // we haven't wrote l1 != null because in the case of breaking of odd sized
+            // LL l2 will be the ptr that will qpproach null first and in even case it will
+            // approach same
+            if (l1.val != l2.val) {
                 res = false;
                 break;
             }
-            
+
             l1 = l1.next;
             l2 = l2.next;
         }
-        
-//         re-attaching the two LL so it become the original list
+
+        // re-attaching the two LL so it become the original list
         newHead = reverseList(newHead);
         mid.next = newHead;
-        
+
         return res;
     }
 
     // LC143
     public void reorderList(ListNode head) {
-        if(head == null || head.next == null) return;
-        
+        if (head == null || head.next == null)
+            return;
+
         ListNode mid = middleNode_01(head), nHead = mid.next;
         mid.next = null;
         nHead = reverseList(nHead);
-        
+
         ListNode c1 = head, c2 = nHead;
-        
-        
-        while(c2 != null){
+
+        while (c2 != null) {
             ListNode fwd1 = c1.next, fwd2 = c2.next;
-            
+
             c1.next = c2;
             c2.next = fwd1;
-            
+
             c1 = fwd1;
             c2 = fwd2;
         }
     }
 
-    public int size(ListNode head){
-        if(head == null) return 0;
+    public int size(ListNode head) {
+        if (head == null)
+            return 0;
         int count = 0;
         ListNode temp = head;
-        while(temp != null) {
+        while (temp != null) {
             temp = temp.next;
-             count++;
+            count++;
         }
         return count;
     }
 
-    public void swap(ListNode l1, ListNode l2){
+    public void swap(ListNode l1, ListNode l2) {
         ListNode temp = new ListNode();
         temp.val = l1.val;
         temp.next = l1.next;
@@ -145,9 +149,9 @@ public class LinkedList01 {
         l2.next = temp.next;
     }
 
-    public ListNode getNodeAtIdx(ListNode node, int idx){
+    public ListNode getNodeAtIdx(ListNode node, int idx) {
         ListNode temp = node;
-        while(idx-- >= 0){
+        while (idx-- >= 0) {
             temp = temp.next;
         }
         return temp;
@@ -155,12 +159,13 @@ public class LinkedList01 {
 
     // LC21
     public ListNode mergeTwoSortedLists(ListNode list1, ListNode list2) {
-        if(list1 == null || list2 == null) return list1 == null ? list2 : list1;
+        if (list1 == null || list2 == null)
+            return list1 == null ? list2 : list1;
 
         ListNode dummy = new ListNode(-1), prev = dummy, ptr1 = list1, ptr2 = list2;
 
-        while (ptr1 != null && ptr2 != null){
-            if(ptr1.val <= ptr2.val){
+        while (ptr1 != null && ptr2 != null) {
+            if (ptr1.val <= ptr2.val) {
                 prev.next = ptr1;
                 ptr1 = ptr1.next;
             } else {
@@ -195,22 +200,23 @@ public class LinkedList01 {
         c1.next = h2;
     }
 
-
     // Leetcode 19
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head == null || n > 32) return null;
+        if (head == null || n > 32)
+            return null;
 
         ListNode slow = head, fast = head;
-        while(n-- > 0) fast = fast.next;
+        while (n-- > 0)
+            fast = fast.next;
 
-        if(fast == null){
+        if (fast == null) {
             ListNode removeNode = head;
             head = head.next;
             removeNode.next = null;
             return head;
         }
 
-        while(fast.next != null){
+        while (fast.next != null) {
             fast = fast.next;
             slow = slow.next;
         }
@@ -223,20 +229,23 @@ public class LinkedList01 {
 
     // Leetcode 2 -> list given is already reversed
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1 == null || l2 == null) return (l1 == null) ? l2 : l1;
-        
+        if (l1 == null || l2 == null)
+            return (l1 == null) ? l2 : l1;
+
         ListNode dummy = new ListNode(-1), prev = dummy, ptr1 = l1, ptr2 = l2;
         int carry = 0;
-        while(ptr1 != null || ptr2 != null || carry != 0){
+        while (ptr1 != null || ptr2 != null || carry != 0) {
             int sum = carry + (ptr1 != null ? ptr1.val : 0) + (ptr2 != null ? ptr2.val : 0);
-            carry = sum/10;
+            carry = sum / 10;
             prev.next = new ListNode(sum % 10);
             prev = prev.next;
-            
-            if(ptr1 != null) ptr1 = ptr1.next;
-            if(ptr2 != null) ptr2 = ptr2.next;
+
+            if (ptr1 != null)
+                ptr1 = ptr1.next;
+            if (ptr2 != null)
+                ptr2 = ptr2.next;
         }
-        
+
         ListNode head = dummy.next;
         dummy.next = null;
         return head;
@@ -244,35 +253,154 @@ public class LinkedList01 {
 
     // Leetcode 445 addTwoNumber2 -> list given is not in reverse order
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1 == null || l2 == null) return (l1 == null) ? l2 : l1;
-        
+        if (l1 == null || l2 == null)
+            return (l1 == null) ? l2 : l1;
+
         l1 = reverseList(l1);
         l2 = reverseList(l2);
-        
+
         ListNode dummy = new ListNode(-1), prev = dummy, ptr1 = l1, ptr2 = l2;
         int carry = 0;
-        while(ptr1 != null || ptr2 != null || carry != 0){
+        while (ptr1 != null || ptr2 != null || carry != 0) {
             int sum = carry + (ptr1 != null ? ptr1.val : 0) + (ptr2 != null ? ptr2.val : 0);
-            carry = sum/10;
+            carry = sum / 10;
             prev.next = new ListNode(sum % 10);
             prev = prev.next;
-            
-            if(ptr1 != null) ptr1 = ptr1.next;
-            if(ptr2 != null) ptr2 = ptr2.next;
+
+            if (ptr1 != null)
+                ptr1 = ptr1.next;
+            if (ptr2 != null)
+                ptr2 = ptr2.next;
         }
-        
+
         l1 = reverseList(l1);
         l2 = reverseList(l2);
-        
+
         ListNode head = dummy.next;
         dummy.next = null;
         head = reverseList(head);
         return head;
     }
 
-    // madeup
+    // gfg subtraction between two LL
+    public int isBiggerList(ListNode l1, ListNode l2) {
+        int len1 = size(l1), len2 = size(l2);
+
+        if (len1 == len2) {
+            ListNode ptr1 = l1, ptr2 = l2;
+            while (ptr1 != null) {
+                if (ptr1.val != ptr2.val)
+                    return ptr1.val - ptr2.val;
+                ptr1 = ptr1.next;
+                ptr2 = ptr2.next;
+            }
+        }
+
+        return len1 - len2; // +ve l1 is big otherwise opposite
+    }
+
     public ListNode subtractTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        if (isBiggerList(l1, l2) < 0) {
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode dummy = new ListNode(-1), prev = dummy, ptr1 = l1, ptr2 = l2;
+
+        int borrow = 0;
+        while (ptr1 != null || ptr2 != null) {
+            int val = borrow + (ptr1 != null ? ptr1.val : 0) - (ptr2 != null ? ptr2.val : 0);
+
+            // updating borrow
+            if (val < 0) {
+                val += 10;
+                borrow = -1;
+            } else {
+                borrow = 0;
+            }
+
+            // adding it to the ans list
+            prev.next = new ListNode(val);
+            prev = prev.next;
+
+            if (ptr1 != null)
+                ptr1 = ptr1.next;
+            if (ptr2 != null)
+                ptr2 = ptr2.next;
+        }
+
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+
+        ListNode head = dummy.next;
+        dummy.next = null;
+
+        head = reverseList(head);
+        ptr1 = head;
+
+        while (ptr1.next != null) {
+            if (ptr1.val != 0) {
+                break;
+            }
+            ptr1 = ptr1.next;
+        }
+
+        head = ptr1;
+        return head;
+    }
+
+    // remove duplicate-
+    // https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/linked-list/remove-duplicate-from-sorted-linkedlist/ojquestion
+    public ListNode removeDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode dummy = new ListNode(-1), prev = dummy, ptr1 = head;
+
+        while (ptr1 != null) {
+            if (ptr1.val != prev.val) {
+                prev.next = new ListNode(ptr1.val);
+                prev = prev.next;
+            }
+            ptr1 = ptr1.next;
+        }
+
+        head = dummy.next;
+        dummy.next = null;
+        return head;
+    }
+
+    // remove all duplicates
+    // https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/linked-list/remove-all-duplicates-from-sorted-linkedlist/ojquestion
+    public ListNode removeAllDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(-1), prev = dummy, curr = head.next;
+        
+        prev.next = head;
+        // prev.next = head beacuse we're considering it as potential ele
+        
+        while(curr != null){
+            boolean isSequence = false;
+            while(curr != null && prev.next.val == curr.val){
+                curr = curr.next;
+                isSequence = true;
+            }
+
+            if(isSequence){
+                prev.next = curr; // now at the end of prev seq. we're making curr ele as potential unique ele
+            } else {
+                // element is unique
+                prev = prev.next;
+            }
+
+            if(curr != null) curr = curr.next;
+        }
+
+        return dummy.next;
     }
 
 }
